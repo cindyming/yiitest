6e7f759afc9084bded0dba7924d7fe3852605f32
Cindy Ming <cindy.ming1988@gmail.com>
2015-11-03 20:47:16 +0800

    add the backup script

---
+++ backup.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+
+cd /home/backup
+
+echo “You are In Backup Directory”
+
+Now=$(date +”%d-%m-%Y--%H:%M:%S”)
+
+File=backup-$Now.sql.gz
+
+mysqldump -uroot  -palks@111 mgjiayuan --add-drop-table | gzip > $File
+
+mysql -uroot -palks@111 afs -e "insert into backup (filename) values ('$File');"
+
+echo “Your Database Backup Successfully Completed”
