1c916b333c8e3e4d250070f09e1a61950226aa2c
Cindy Ming <cindy.ming1988@gmail.com>
2015-11-16 21:42:40 +0800

    fix user for

--- views/user/adminupdate.php
+++ views/user/adminupdate.php
@@ -5,7 +5,7 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\User */
 
-$this->title = '修改會員資料 ';
+$this->title = '修改会员资料 ';
 $this->params['breadcrumbs'][] = ['label' => 'Users', 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->title, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = 'Update';
