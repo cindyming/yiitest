e0303eda37accb53b824c678da302201378c73bd
Cindy Ming <cindy.ming1988@gmail.com>
2015-11-24 19:26:40 +0800

    fix

--- models/CashSearch.php
+++ models/CashSearch.php
@@ -79,7 +79,7 @@ class CashSearch extends Cash
 
     public function searchForAdmininex($params)
     {
-        $query = Cash::find()->where(['in', 'type', array(1,2,3)])->orderBy(['id' => SORT_DESC]);
+        $query = Cash::find()->where(['in', 'type', array(1,2,3)])->orderBy(['id' => SORT_DESC, 'status' => SORT_ASC]);
 
         // add conditions that should always apply here
 
