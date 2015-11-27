2426ce7912efbe4628fc524a0c5faae3d85022cd
Cindy Ming <cindy.ming1988@gmail.com>
2015-11-27 20:29:14 +0800

    fix

--- vendor/yiisoft/yii2/widgets/LinkPager.php
+++ vendor/yiisoft/yii2/widgets/LinkPager.php
@@ -193,7 +193,8 @@ class LinkPager extends Widget
             $buttons[] = $this->renderPageButton($lastPageLabel, $pageCount - 1, $this->lastPageCssClass, $currentPage >= $pageCount - 1, false);
         }
 
-        return Html::tag('ul', implode("\n", $buttons), $this->options);
+        return Html::tag('ul', implode("\n", $buttons), $this->options) . '<input id="pageNo"/><button onClick="var hurl=\'' . $this->pagination->createUrl(1) . '\';window.location=(hurl).replace(\'page=2\', \'page=\' + parseInt(jQuery(\'#pageNo\').val()));
+">GO</button>';
     }
 
     /**
