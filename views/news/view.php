035fdb4d5bfa6701fd415cd10a490781d5f9d42f
Cindy Ming <cindy.ming1988@gmail.com>
2016-01-01 10:45:13 +0800

    fix news

--- views/news/view.php
+++ views/news/view.php
@@ -10,21 +10,13 @@ $this->title = '公告详细';
 ?>
 <div class="news-view">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <h1><?= Html::encode($model->title) ?></h1>
 
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'id',
-            'be_top',
-            'title',
-            'content:ntext',
-            'created_at',
-            'updated_at',
-            'public_at',
-        ],
-    ]) ?>
+    <div class="news-date"><?php echo ($model->public_at) ? $model->public_at : $model->created_at?></div>
+    <div class="new-content">
+        <?php echo $model->content?>
 
+    </div>
     <p>
         <?= Html::a('返回', ['index'], ['class' => 'btn btn-primary']) ?>
     </p>
