513f01ea4236b8179a17f99c2b15c017eede994c
cindy <cindy@jingdigital.co>
2016-10-08 10:10:07 +0800

    fix

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -62,6 +62,9 @@ $isAdmin = Yii::$app->user->identity->isAdmin();
                 ?>
             </div>
         <?php endif ?>
+        <?php if (!System::loadConfig('maintenance')) :?>
+        <div class="alert alert-danger">系统维护中，请不要操作任何数据，您的操作记录不会被保存</div>
+        <?php endif ?>
         <!-- Breadcrumbs::widget([
             'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
         ]) -->
