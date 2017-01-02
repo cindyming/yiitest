2d8c4ba8c5663fb32b6ce6735df78581684626fb
cindy <cindy@jingdigital.co>
2017-01-02 23:08:47 +0800

    fix

--- models/Cash.php
+++ models/Cash.php
@@ -47,6 +47,18 @@ class Cash extends ActiveRecord
             [
                 'class' => AttributeBehavior::className(),
                 'attributes' => [
+                    ActiveRecord::EVENT_BEFORE_INSERT => 'cardnumber',
+                ],
+                'value' => function ($event) {
+                    if ($this->cardnumber) {
+                        return trim(Yii::$app->user->identity->cardnumber);
+                    }
+
+                },
+            ],
+            [
+                'class' => AttributeBehavior::className(),
+                'attributes' => [
                     ActiveRecord::EVENT_BEFORE_INSERT => 'real_amount',
                 ],
                 'value' => function ($event) {
