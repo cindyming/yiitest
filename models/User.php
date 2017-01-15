199c06f2b06092791a3f2709024751bc336ee483
cindy <cindy@jingdigital.co>
2017-01-15 18:23:59 +0800

    fix

--- models/User.php
+++ models/User.php
@@ -494,10 +494,14 @@ class User extends ActiveRecord implements IdentityInterface
 
     public function diamondLevel()
     {
-         $users = User::find()->where(['=', 'referer', $this->id])->andWhere(['=', 'role_id', 3])->andWhere(['=', 'merited', 1])->orderBy(['achievements' => SORT_ASC])->limit(3)->all();
+         $users = User::find()->where(['=', 'referer', $this->id])->andWhere(['=', 'role_id', 3])->andWhere(['=', 'merited', 1])->orderBy(['id' => SORT_ASC])->limit(3)->all();
 
         if (count($users) == 3 ) {
-            return $users[0]->achievements;
+            $a = 10000000000;
+            foreach ($users as $u) {
+                $a = ($a > $u->achievements) ? $u->achievements : $a;
+            }
+            return $a;
         } else {
             return 0;
         }
