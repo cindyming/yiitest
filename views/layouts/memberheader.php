81ed7476aaf74369d751342c459daad21431a738
zy诸琰 <zhu.yana@dst59474.cn1.global.ctrip.com>
2017-08-31 14:21:38 +0800

    hidden header

--- views/layouts/memberheader.php
+++ views/layouts/memberheader.php
@@ -25,34 +25,33 @@ $welcome .= ', 欢迎回来.';
             <li>
                 网络昵称: <?php echo Yii::$app->user->identity->username?>
             </li>
-           
-            <li>
+            <li class="hidden">
                 总投资额: <?php echo Yii::$app->user->identity->investment ?>
                 <?php if (\app\models\System::loadConfig('open_stack_transfer')): ?>
                 (等值股票数: <?php echo (Yii::$app->user->identity->stack)?>)
             </li>
-            <!-- <li>
+            <li class="hidden">
                 总业绩: <?php echo Yii::$app->user->identity->achievements?>
-            </li> -->
-            <!-- <li>
+            </li>
+            <li class="hidden">
                 总股票数: <?php echo Yii::$app->user->identity->getTotalStack() ?>
                 <?php endif ?>
-            </li>-->
-            <!-- <li>
+            </li>
+            <li class="hidden">
                 分红余额: <?php echo Yii::$app->user->identity->bonus_remain?>
             </li>
-            <li>
+            <li class="hidden">
                 服务费余额: <?php echo round(Yii::$app->user->identity->baodan_remain, 2)?>
             </li>
-            <li>
+            <li class="hidden">
                 可提现绩效工资余额: <?php echo Yii::$app->user->identity->merit_remain?>
             </li>
-            <li>
+            <li class="hidden">
                 商城币余额: <?php echo round(Yii::$app->user->identity->mall_remain, 2)?>
             </li>
-            <li>
+            <li class="hidden">
                 对冲帐户余额: <?php echo round(Yii::$app->user->identity->duichong_remain, 2)?>
-            </li> -->
+            </li>
         </ul>
         <?php if (\app\models\System::loadConfig('open_stack_transfer')): ?>
             <ul>
