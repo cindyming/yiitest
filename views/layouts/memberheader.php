c4956a33f517ea860b8cae9bfeb0b12435f7ba2d
cindy <cindy@jingdigital.co>
2017-09-02 20:23:15 +0800

    fix

--- views/layouts/memberheader.php
+++ views/layouts/memberheader.php
@@ -25,11 +25,12 @@ $welcome .= ', 欢迎回来.';
             <li>
                 网络昵称: <?php echo Yii::$app->user->identity->username?>
             </li>
-            <?php if(\app\models\System::loadConfig('show_total')):?>
+
                 <li class="">
                 总业绩: <?php echo Yii::$app->user->identity->achievements?>
             </li>
 
+            <?php if(\app\models\System::loadConfig('show_total')):?>
             <li>
                 总投资额: <?php echo Yii::$app->user->identity->investment ?>
                <?php if (\app\models\System::loadConfig('open_stack_transfer')): ?>
